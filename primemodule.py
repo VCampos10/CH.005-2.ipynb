70bcd84b0d7aa1179380e9cbf68dfb7f299c7dce
VCampos10 <VC195063@icloud.com>
2024-10-18 22:54:14 -0700

    update

--- primemodule.py
+++ primemodule.py
@@ -1,6 +1,6 @@
 from primemodule import is_prime, print_primes, get_primes
 #is_prime(10)
-def is_prime (10):
+def is_prime (n):
     if 10 <= 1:
         return False
     for i in range(2, 10):
@@ -9,16 +9,16 @@ def is_prime (10):
         return True
     
 
-def print_primes(10):
+def print_primes(n):
     for i in range(2, 10):
         if is_prime(i):
             print(i, end=' ')
         print()
 ##print_primes(10)
 
-
-get_primes(10)
-def get_primes(10):
+#def gen_lambda
+#get_primes(n)
+#def get_primes(n):
     
 
 
